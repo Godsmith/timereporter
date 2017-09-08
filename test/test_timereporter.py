8dab17737f1f97bf31b3c59aa42ef6d847f57829
Godsmith <filip.lange@gmail.com>
2017-09-08 15:54:25 +0200

    Refactored tests, not code yet

--- test/test_timereporter.py
+++ test/test_timereporter.py
@@ -6,38 +6,71 @@ from timereporter import DayError
 
 today = date.today().isoformat()
 
-class TestDay:
-    def test_no_lunch(self):
-        t = TimeReporter('09:00 15:00'.split())
+class TestTimeReporter:
+    def test_standard(self):
+        t = TimeReporter(args='9 15'.split())
         assert str(t.days[today].came) == '09:00:00'
         assert str(t.days[today].went) == '15:00:00'
 
+class TestDay:
+    def test_no_lunch(self):
+        d = Day('09:00 15:00'.split())
+        assert str(d.came) == '09:00:00'
+        assert str(d.went) == '15:00:00'
+
     def test_different_formatting(self):
-        t = TimeReporter('9:00 15:00'.split())
-        assert str(t.days[today].came) == '09:00:00'
-        assert str(t.days[today].went) == '15:00:00'
+        d = Day('9:00 15:00'.split())
+        assert str(d.came) == '09:00:00'
+        assert str(d.went) == '15:00:00'
         
-        t = TimeReporter('9 15:00'.split())
-        assert str(t.days[today].came) == '09:00:00'
-        assert str(t.days[today].went) == '15:00:00'
+        d = Day('9 15:00'.split())
+        assert str(d.came) == '09:00:00'
+        assert str(d.went) == '15:00:00'
         
-        t = TimeReporter('9 15'.split())
-        assert str(t.days[today].came) == '09:00:00'
-        assert str(t.days[today].went) == '15:00:00'
+        d = Day('9 15'.split())
+        assert str(d.came) == '09:00:00'
+        assert str(d.went) == '15:00:00'
         
-        t = TimeReporter('9 15'.split())
-        assert str(t.days[today].came) == '09:00:00'
-        assert str(t.days[today].went) == '15:00:00'
+        d = Day('9 15'.split())
+        assert str(d.came) == '09:00:00'
+        assert str(d.went) == '15:00:00'
 
     def test_went_before_came(self):
         with pytest.raises(DayError):
-            t = TimeReporter('15:00 09:00'.split())
+            d = Day('15:00 09:00'.split())
             
     def test_whole_day_with_lunch(self):
-        t = TimeReporter('9 15 45min'.split())
-        assert str(t.days[today].came) == '09:00:00'
-        assert str(t.days[today].went) == '15:00:00'
-        assert str(t.days[today].lunch) = '0:45:00'
+        d = Day('9 15 45min'.split())
+        assert str(d.came) == '09:00:00'
+        assert str(d.went) == '15:00:00'
+        assert str(d.lunch) = '0:45:00'
+       
+    def test_only_came(self):
+        d = Day('9'.split())
+        assert str(d.came) == '09:00:00'
+        assert str(d.went) == None
+        assert str(d.lunc) == None
+        
+        d = Day('09:00'.split())
+        assert str(d.came) == '09:00:00'
+        
+        d = Day('9:00'.split())
+        assert str(d.came) == '09:00:00'
+        
+    def test_only_lunch(self):
+        d = Day('45min'.split())
+        assert str(d.came) == None
+        assert str(d.went) == None
+        assert str(d.lunch) = '0:45:00'
+        
+        d = Day('45m'.split())
+        assert str(d.lunch) = '0:45:00'
+        
+        d = Day('45 m'.split())
+        assert str(d.lunch) = '0:45:00'
+        
+        d = Day('45min'.split())
+        assert str(d.lunch) = '0:45:00'
         
      # Plan: instead of creating a TimeReporter object, create a Day object from the command line arguments.
      # Then add the Day object to the Calendar object loaded by the timereporter. Call TimeReporter as so:
@@ -47,14 +80,7 @@ class TestDay:
 
 """Test cases yet to be written
  
-Bara lunch
-t 45m
-t 45 m
-t 45 min
-t 45min
  
-Bara came
-t 9:00
  
 Bara went
 t 15:00 # came before
