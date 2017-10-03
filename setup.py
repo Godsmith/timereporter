cd30a488df8a9986d71e2d7545e9a5c9b0c88169
Filip Lange <filip.lange@gmail.com>
2017-10-03 22:47:14 +0200

    Travis test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='timereporter',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/Godsmith/timereporter',
+    license='',
+    author='Filip',
+    author_email='',
+    description=''
+)
