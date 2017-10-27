5b9a8845cb80c4725dc32420446e33b1bcaa2d1d
Filip Lange <filip.lange@gmail.com>
2017-10-27 16:34:05 +0200

    Refactoring

--- test/test_bugs.py
+++ test/test_bugs.py
@@ -6,12 +6,12 @@ from timereporter.timereporter import TimeReporter
 
 class TestCrash:
     def test_no_crash_anymore(self, custom_log_path):
-        custom_log_path(Path(os.path.realpath(__file__)).parent / 'fixtures' /
+        custom_log_path(Path(os.path.realpath(__file__)).parent / '_fixtures' /
                         'crash.yaml')
         TimeReporter()
 
     def test_wrong_sum(self, custom_log_path, mock_browser, mockdate_oct_24):
-        custom_log_path(Path(os.path.realpath(__file__)).parent / 'fixtures' /
+        custom_log_path(Path(os.path.realpath(__file__)).parent / '_fixtures' /
                         'wrong_sum.yaml')
         TimeReporter('show week html')
         with open(mock_browser.url) as f:
