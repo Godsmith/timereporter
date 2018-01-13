d53f6e2e83e5b3e4e6b678e1e1de2352ade3793e
Filip Lange <filip.lange@gmail.com>
2018-01-13 21:40:26 +0100

    Fix callgraph always running when running tests

--- callgraph.py
+++ callgraph.py
@@ -7,20 +7,19 @@ from pycallgraph.output import GraphvizOutput
 
 from timereporter.__main__ import main
 
+if __name__ == '__main__':
+    config = Config()
+    config.trace_filter = GlobbingFilter(exclude=[
+        'pycallgraph.*',
+        '_gcd_import',
+        '_lock_unlock_module',
+        '_ModuleLock',
+        'cb',
+        '_sanity_check',
+        '*_dump_*',
+        '*mydatetime*'
+    ])
 
-config = Config()
-config.trace_filter = GlobbingFilter(exclude=[
-    'pycallgraph.*',
-    '_gcd_import',
-    '_lock_unlock_module',
-    '_ModuleLock',
-    'cb',
-    '_sanity_check',
-    '*_dump_*',
-    '*mydatetime*'
-])
-
-graphviz = GraphvizOutput(output_file='pycallgraph.png')
-
-with PyCallGraph(output=graphviz, config=config):
-    main()
+    graphviz = GraphvizOutput(output_file='pycallgraph.png')
+    with PyCallGraph(output=graphviz, config=config):
+        main()
